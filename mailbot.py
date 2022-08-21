bb39457537bda4642a18f3551411609cfdc9b4be
Szymon Jan Piwowar <szymonjanpiwowar@gmail.com>
2022-08-21 12:43:44 +0100

    Reformatted the .py files -> removed minor and major warnings.

--- mailbot.py
+++ mailbot.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
 
-import sys
-import smtplib
-import ssl
-import aiohttp
-from os import environ, getcwd, path
-from email.message import EmailMessage
-import requests, json, datetime
-from os import getcwd, path
-
-def overwrite_json(json_path, jsonObject):
+def overwrite_json(json_path, json_object):
     import json
     json_file = open(json_path, 'w')
-    json.dump(jsonObject, json_file)
+    json.dump(json_object, json_file)
     json_file.close()
 
+
 def read_from_json_file(json_path):
+    import json
+    from os import path
     data = []
     if not path.exists(json_path):
-        return []
+        return data
     else:
         with open(json_path, 'r') as f:
             data = json.loads(f.read())
     return data
 
-def json_exrtract(jsonObject, keys):
+
+def json_extract(json_object, keys):
     """
 
-    :param jsonObject: A list of dictionaries
-    :param key: A data field to be extracted from the jsonObject
+    :param json_object: A list of dictionaries
+    :param keys: A data fields to be extracted from the json_object
     :return: a list of found items
     """
     found = []
-    for entry in jsonObject:
+    for entry in json_object:
         try:
             found_entry = []
             for key in keys:
@@ -42,12 +37,10 @@ def json_exrtract(jsonObject, keys):
             else:
                 found.append(found_entry)
         except KeyError:
-            print("ERROR: Key is not in the JsonObject. Try different key.")
+            print("ERROR: Key is not in the json_object. Try different key.")
             return []
     return found
 
-def update_json_file(file, jsonObject):
-    pass
 
 def gmail_authenticate():
     import pickle
@@ -62,7 +55,7 @@ def gmail_authenticate():
     if path.exists("token.pickle"):
         with open("token.pickle", "rb") as token:
             creds = pickle.load(token)
-    # if there are no (valid) credentials availablle, let the user log in.
+    # if there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -73,6 +66,7 @@ def gmail_authenticate():
         with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
     return build('gmail', 'v1', credentials=creds)
+
 
 def search_messages(service, query):
     result = service.users().messages().list(userId='me', q=query).execute()
@@ -86,13 +80,14 @@ def search_messages(service, query):
             messages.extend(result['messages'])
     return messages
 
+
 def parse_parts(service, parts, folder_name, message):
     """
     Utility function that parses the content of an email partition
     """
     # for encoding/decoding messages in base64
     from os import path
-    from base64 import urlsafe_b64decode, urlsafe_b64encode
+    from base64 import urlsafe_b64decode
     if parts:
         for part in parts:
             filename = part.get("filename")
@@ -135,6 +130,7 @@ def parse_parts(service, parts, folder_name, message):
                                 with open(filepath, "wb") as f:
                                     f.write(urlsafe_b64decode(data))
 
+
 # utility functions
 def get_size_format(b, factor=1024, suffix="B"):
     """
@@ -149,9 +145,11 @@ def get_size_format(b, factor=1024, suffix="B"):
         b /= factor
     return f"{b:.2f}Y{suffix}"
 
+
 def clean(text):
     # clean text for creating a folder
     return "".join(c if c.isalnum() else "_" for c in text)
+
 
 def read_message(service, message):
     """
@@ -162,13 +160,13 @@ def read_message(service, message):
         - Downloads text/html content (if available) and saves it under the folder created as index.html
         - Downloads any file that is attached to the email and saves it in the folder created
     """
-    from os import path, mkdir
+    from os import path
     msg = service.users().messages().get(userId='me', id=message['id'], format='full').execute()
     # parts can be the message body, or attachments
     payload = msg['payload']
     headers = payload.get("headers")
     parts = payload.get("parts")
-    folder_name = "email"
+    folder_name, email_from = "email", ""
     has_subject = False
     if headers:
         # this section prints email basic info & creates a folder for the email
@@ -222,17 +220,17 @@ def delete_messages(service, query):
         }
     ).execute()
 
-def check_unsubscribed(distribution_list):
+
+def check_unsubscribed():
     """
         Checks for incoming unsubscribe emails. Updates the .distribution_list file.
     """
-    from os import path
     # Request all access (permission to read/send/receive emails, manage the inbox, and more)
-    fpl_username = r'jacobsfantasypremierleague@gmail.com'
     query = r'in:inbox Unsubscribe'
     service = gmail_authenticate()
     results = search_messages(service, query)
     managers_unsubscribed = []
+    from_email = ""
     if not results:
         print("No unsubscribe emails found!")
         return []
@@ -248,17 +246,16 @@ def check_unsubscribed(distribution_list):
     delete_messages(service, query)
     return managers_unsubscribed
 
-def check_update(bootstrap, fixtures, jsonObject):
+
+def check_update(bootstrap, fixtures, json_object):
     """
         A function that will be triggered to distribute emails to all of the league
         participants.
 
-        The fucntion will take todays date and compare it to the
+        The function will take today's date and compare it to the
     """
-    import json
     from datetime import datetime
-    up_file = 'mail_data/.league_data.json'
-    gameweek_data = jsonObject['league_data'][1]['gameweek_info']
+    gameweek_data = json_object['league_data'][1]['gameweek_info']
     days_to_deadline = bootstrap.get_days_to_deadline()
     current_gw = bootstrap.get_current_gameweek()
     upcoming_gw = current_gw + 1
@@ -266,7 +263,7 @@ def check_update(bootstrap, fixtures, jsonObject):
     has_reminded, has_newscast = False, False
     send_reminder, send_newsletter = False, False
     # extract info on whether, or not the reminder has been sent out to the
-    # users for the upcoming gamweek.
+    # users for the upcoming gameweek.
     # Also, extract the data about the current gameweek reminder for newsletter
     for gw_entry in gameweek_data:
         # for newsletter
@@ -280,23 +277,23 @@ def check_update(bootstrap, fixtures, jsonObject):
     if days_to_deadline <= 1.0 and not has_reminded:
         send_reminder = True
         upgw_index = upcoming_gw + 1
-        jsonObject['league_data'][1]['gameweek_info'][upgw_index]['reminder_sent'] \
+        json_object['league_data'][1]['gameweek_info'][upgw_index]['reminder_sent'] \
             = datetime.now().strftime('%d/%M/%YT%H:%M')
     # update newsletter reminder
     if time_to_final_gw_fixture < 0.0 and not has_newscast:
         send_newsletter = True
         crgw_index = current_gw + 1
-        jsonObject['league_data'][1]['gameweek_info'][crgw_index]['newsletter_sent'] \
+        json_object['league_data'][1]['gameweek_info'][crgw_index]['newsletter_sent'] \
             = datetime.now().strftime('%d/%M/%YT%H:%M')
     return send_reminder, send_newsletter, days_to_deadline, time_to_final_gw_fixture
+
 
 def read_newsletter_email_body(league, bootstrap, players):
     """
         Reads data from ./mail_data/body.txt
     """
-    from datetime import datetime
+    from os import getcwd, path
     # Obtain manager related data and format!
-    no_managers = league.managers.no_managers
     table_of_managers = league.managers.create_manager_table(bootstrap).to_html(index=False,
                                                                                 justify='center',
                                                                                 )
@@ -304,46 +301,56 @@ def read_newsletter_email_body(league, bootstrap, players):
     table_of_managers = table_of_managers.replace("]", "")
     table_of_managers = table_of_managers.replace(">False, 0", " bgcolor=\"#337580\">")
     table_of_managers = table_of_managers.replace(">True, ", " bgcolor=\"#bc909a\"> Used on GW #")
-    # Top 5 players on each positon, based on historic data...
-    top5_gks = players.get_player_data('Goalkeeper', 'cost_to_points', False, 5).to_html(index=False,
-                                                                                justify='center',
-                                                                                float_format='{:.2f}'.format
-                                                                                )
-    top5_defs = players.get_player_data('Defender', 'cost_to_points', False, 5).to_html(index=False,
-                                                                                justify='center',
-                                                                                float_format='{:.2f}'.format
-                                                                                )
-    top5_mids = players.get_player_data('Midfielder', 'cost_to_points', False, 5).to_html(index=False,
-                                                                                justify='center',
-                                                                                float_format='{:.2f}'.format
-                                                                                )
-    top5_frw = players.get_player_data('Forward', 'cost_to_points', False, 5).to_html(index=False,
-                                                                                justify='center',
-                                                                                float_format='{:.2f}'.format
-                                                                                )
+    # Top 5 players on each position, based on historic data...
+    top5_goalkeepers = players.get_player_data('Goalkeeper',
+                                               'cost_to_points',
+                                               False, 5).to_html(index=False,
+                                                                 justify='center',
+                                                                 float_format='{:.2f}'.format
+                                                                 )
+    top5_defenders = players.get_player_data('Defender',
+                                             'cost_to_points',
+                                             False, 5).to_html(index=False,
+                                                               justify='center',
+                                                               float_format='{:.2f}'.format
+                                                               )
+    top5_midfielders = players.get_player_data('Midfielder',
+                                               'cost_to_points',
+                                               False, 5).to_html(index=False,
+                                                                 justify='center',
+                                                                 float_format='{:.2f}'.format
+                                                                 )
+    top5_forwards = players.get_player_data('Forward',
+                                            'cost_to_points',
+                                            False, 5).to_html(index=False,
+                                                              justify='center',
+                                                              float_format='{:.2f}'.format
+                                                              )
     mail_body = ''
     with open(path.join(getcwd(), 'mail_data', 'newsletter_body.txt'), 'r') as bod:
         for line in bod.readlines():
             if "@players_table" in line:
                 line = line.replace("@players_table", table_of_managers)
             if "@gk_rec" in line:
-                line = line.replace("@gk_rec", top5_gks)
+                line = line.replace("@gk_rec", top5_goalkeepers)
             if "@def_rec" in line:
-                line = line.replace("@def_rec", top5_defs)
+                line = line.replace("@def_rec", top5_defenders)
             if "@mid_rec" in line:
-                line = line.replace("@mid_rec", top5_mids)
+                line = line.replace("@mid_rec", top5_midfielders)
             if "@for_rec" in line:
-                line = line.replace("@for_rec", top5_frw)
+                line = line.replace("@for_rec", top5_forwards)
             mail_body += line + "\n"
     return mail_body
 
-def read_email_body(league, bootstrap):
+
+def read_email_body(league):
     """
         Reads data from ./mail_data/body.txt
     """
     from datetime import datetime
+    from os import getcwd, path
     to_league_start = datetime(2022, 8, 20).timestamp() - datetime.today().timestamp()
-    to_league_start = round(to_league_start/86400.0, 0)
+    to_league_start = round(to_league_start / 86400.0, 0)
     no_managers = league.managers.no_managers
     mail_body = ""
     with open(path.join(getcwd(), 'mail_data', 'body.txt'), 'r') as bod:
@@ -359,12 +366,15 @@ def read_email_body(league, bootstrap):
             mail_body += line + "\n"
     return mail_body
 
+
 def send_reminder_email(distribution_list, subject, html_body, files_attached):
     from pathlib import Path
     from email.mime.multipart import MIMEMultipart
     from email.mime.text import MIMEText
     from email.mime.base import MIMEBase
     from email import encoders
+    from os import environ
+    import smtplib
     fpl_username = r'jacobsfantasypremierleague@gmail.com'
     fpl_password = environ.get('FPL_GM_PASS')
     files = files_attached
@@ -396,37 +406,44 @@ def send_reminder_email(distribution_list, subject, html_body, files_attached):
             smtp.sendmail(fpl_username, email_address, mail.as_string())
             smtp.quit()
 
+
 def main():
     from classic_league import BootStrap
     from classic_league import ClassicLeague
     from classic_league import Players
     from classic_league import Fixtures
-    from pandas import DataFrame, read_json, json_normalize
-    from datetime import datetime
+    from os import getcwd, path
     json_path = path.join(getcwd(), 'mail_data/.league_data.json')
-    jsonObject = read_from_json_file(json_path)
-    manager_data = jsonObject['league_data'][0]['manager_data']
-    distribution_list = json_exrtract(manager_data, ['email'])
-    managers_unsubscribed = check_unsubscribed(distribution_list)
+    json_object = read_from_json_file(json_path)
+    manager_data = json_object['league_data'][0]['manager_data']
+    distribution_list = json_extract(manager_data, ['email'])
+    managers_unsubscribed = check_unsubscribed()
+    exit()
     for unsubscribed_email in managers_unsubscribed:
         for index, entry in enumerate(manager_data):
             if entry['email'] == unsubscribed_email:
-                print(index)
-                del jsonObject['league_data'][0]['manager_data'][index]
+                del json_object['league_data'][0]['manager_data'][index]
                 break
     league = ClassicLeague(1026637, 'Jacobs FPL S4')
     bootstrap = BootStrap()
     fixtures = Fixtures(bootstrap)
+    current_gw = bootstrap.get_current_gameweek()
+    upcoming_gw = current_gw + 1
     if not distribution_list:
         print("No one to send emails to...")
-        overwrite_json(json_path, jsonObject)
+        overwrite_json(json_path, json_object)
         exit(0)
-    send_reminder, send_newsletter, days_to_deadline, days_to_final_game = check_update(bootstrap, fixtures, jsonObject)
+    send_reminder, send_newsletter, days_to_deadline, days_to_final_game = check_update(bootstrap,
+                                                                                        fixtures,
+                                                                                        json_object
+                                                                                        )
     hours_to_deadline = round(days_to_deadline * 24.0, 0)
     # Check if update email should be sent out to the managers
     if send_reminder:
-        reminder_email_body = read_email_body(league, bootstrap)
-        subject = "REMINDER - Only {} h Remain! Update your team for the Gameweek #{}!".format(hours_to_deadline, upcoming_gw)
+        reminder_email_body = read_email_body(league)
+        subject = "REMINDER - Only {} h Remain! Update your team for the Gameweek #{}!".format(hours_to_deadline,
+                                                                                               upcoming_gw
+                                                                                               )
         attachments = ['./mail_data/images/Banner.png']
         send_reminder_email(distribution_list, subject, reminder_email_body, attachments)
     else:
@@ -440,7 +457,10 @@ def main():
         attachments = ['./mail_data/images/Newsletter_Banner.png']
         send_reminder_email(distribution_list, subject, newsletter_body, attachments)
     else:
-        print("It's not yet time to sent newsletter email! Days to/since final GW fixture: {}".format(days_to_final_game/86400.0))
-    overwrite_json(json_path, jsonObject)
+        print("It's not yet time to sent newsletter email! "
+              "Days to/since final GW fixture: {}".format(days_to_final_game / 86400.0))
+    overwrite_json(json_path, json_object)
+
+
 if __name__ == "__main__":
     main()
